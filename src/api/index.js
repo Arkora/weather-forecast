965d713319b35ffe56b488ee43ab5a4c96a2bb18
arkora <rapolari98@hotmail.com>
2022-10-30 02:28:02 +0300

    add api key

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+
 const API = axios.create({ baseURL: 'https://api.openweathermap.org/data/2.5' })
 
-const key = 'febc97a3a3906cbd033f05dcce77cd4c'
+const key = process.env.REACT_APP_API_KEY
+
 
 export const getByLocation = (lat,lng) => API.get(`/weather?lat=${lat}&lon=${lng}&appid=${key}&units=metric`)
  export const getByCity = (term) => API.get(`/weather?q=${term}&appid=${key}&units=metric`)
